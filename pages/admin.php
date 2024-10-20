fa352b6cc12b6d25eee411c329c38ef79ebaca79
bonitoflakes101 <xgrendonila2020@plm.edu.ph>
2024-10-21 00:24:30 +0800

    admin logo fix

--- pages/admin.php
+++ pages/admin.php
@@ -96,7 +96,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title>
+    <title>TechVault</title>
+    <link
+        rel="icon"
+        type="image/x-icon"
+        href="../resources/images/logo-icon.png" />
     <script src="../js/admin.js"></script>
 
     <!-- <link rel="stylesheet" href="../css/mainStyle.css" /> -->
